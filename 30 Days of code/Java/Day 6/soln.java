d19c20425e59dc1c575803a0f2853f07874c8452
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-01 18:19:03 +0530

    Create soln.java

---
+++ 30 Days of code/Java/Day 6/soln.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        for (int i = 0; i < T; i++) {
+            String str = scan.next();
+            printEvensOdds(str);
+        }
+        scan.close();
+    }
+    public static void printEvensOdds(String str) {
+        StringBuffer evens = new StringBuffer();
+        StringBuffer odds  = new StringBuffer();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (i % 2 == 0) {
+                evens.append(ch);
+            } else {
+                odds.append(ch);
+            }
+        }
+        System.out.println(evens + " " + odds);
+    }
+}
