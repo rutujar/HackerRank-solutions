91e26c5956109ff66db7d9c882c53667db3a960b
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-02 11:19:15 +0530

    Create soln.java

---
+++ 30 Days of code/Java/Day 20/soln.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+ private static int swaps = 0;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int [] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] =  scan.nextInt();
+        }
+        scan.close();
+        
+        bubbleSort(array);
+        
+        System.out.println("Array is sorted in " + swaps + " swaps.");
+        System.out.println("First Element: " + array[0]);
+        System.out.println("Last Element: " + array[array.length - 1]);
+    }
+    
+    private static void bubbleSort(int [] array) {
+        if (array == null) {
+            return;
+        }
+        boolean swapped = true;
+        int endOffset = 0; // optimizes code
+        while (swapped) {
+            swapped = false;
+            for (int i = 1; i < array.length - endOffset; i++) {
+                if (array[i-1] > array[i]){
+                    swap(array, i-1, i);
+                    swapped = true;
+                }
+            }
+            endOffset++;
+        }
+    }
+    
+    /* Standard swap. Also updates our "swaps" variable */
+    private static void swap(int [] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        swaps++;
+    }
+}
