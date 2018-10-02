9cf385314f12e85564ee06991cba9934c16f9919
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-02 11:16:09 +0530

    Create soln.java

---
+++ 30 Days of code/Java/Day 14/soln.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+	// Add your code here
+    Difference (int [] elements) {
+        this.elements = elements;
+    }
+
+    //          Runtime: O(n)
+    // Space Complexity: O(1)
+    void computeDifference() {
+        int max = Arrays.stream(elements).max().getAsInt();
+        int min = Arrays.stream(elements).min().getAsInt();     
+        maximumDifference = max - min ;
+    }
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
