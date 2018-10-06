3fa49cd0ea28af7e18382ecaa2a59d8245e06b74
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-06 15:24:30 +0530

    Create Bitwise_Operators.js

---
+++ 10 Days of Javascript/Day 6/Bitwise_Operators.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getMaxLessThanK(n, k) {
+    let maximum = 0;
+    let current = -1;
+    for (let i = 1; i < n; i++) {
+        for (let j = i + 1; j <= n; j++) {
+            current = i & j;
+            if (current < k && current > maximum) {
+                maximum = current;
+            }
+        }
+    }
+    return maximum;
+}
+
+function main() {
+    const q = +(readLine());
+    
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
