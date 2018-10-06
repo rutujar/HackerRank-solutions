2db79a6ba0e27a1109b7b6e5e953d5a4f385b713
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-06 15:26:32 +0530

    Create Javascript_Dates.js

---
+++ 10 Days of Javascript/Day 6/Javascript_Dates.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+
+    var day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    var date = new Date(dateString);
+
+    dayName = day_names[date.getDay()];
+
+    return dayName;
+}
+
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}
