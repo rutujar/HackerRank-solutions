04afe300a96fbc66f6000aca211ebe7a1b7d66cf
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-07 09:36:48 +0530

    Create soln.java

---
+++ 30 Days of code/Java/Day 24/soln.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+static void levelOrder(Node root){
+      if (root == null) {
+            return;
+        }
+        ArrayDeque<Node> deque = new ArrayDeque<>(); // use deque as a queue
+        deque.add(root);
+        while (!deque.isEmpty()) {
+            Node n = deque.removeFirst();
+            System.out.print(n.data + " ");
+            if (n.left != null) {
+                deque.addLast(n.left);
+            }
+            if (n.right != null) {
+                deque.addLast(n.right);
+            }
+        }
+    }
+public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            levelOrder(root);
+        }	
+}
