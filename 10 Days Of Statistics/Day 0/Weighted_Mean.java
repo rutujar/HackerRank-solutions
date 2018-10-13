b0e145675ff2cd2f08d574748d49dfc6c1f358ae
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-13 16:00:41 +0530

    Create Weighted_Mean.java

---
+++ 10 Days Of Statistics/Day 0/Weighted_Mean.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        /* Save input */
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int [] elements = new int[size];
+        int [] weights  = new int[size];
+        for (int i = 0; i < size; i++) {
+            elements[i] = scan.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            weights[i] = scan.nextInt();
+        }
+        scan.close();
+        
+        /* Calculate weighted mean */
+        int total = 0;
+        int totalWeights = 0;
+        for (int i = 0; i < size; i++) {
+            total += elements[i] * weights[i];
+            totalWeights += weights[i];
+        }
+        double weightedMean = (double) total / totalWeights;
+        System.out.format("%.1f", weightedMean);
+    }
+}
