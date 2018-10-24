71b7569f3b7c363d0a4cdd5e1246889b0f9568e8
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-24 14:10:42 +0530

    Create soln.java

---
+++ Java/Introduction/Java Stdin and Stdout II/soln.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Read input */
+        Scanner scan = new Scanner(System.in);
+        int i    = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();              // gets rid of the pesky newline
+        String s = scan.nextLine();
+        scan.close();
+        
+        /* Print output */
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
+
