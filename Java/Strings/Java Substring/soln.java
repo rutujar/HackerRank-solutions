aa3e37017b16e554a44b04fc2f9252506a5e8325
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-25 19:14:26 +0530

    Create soln.java

---
+++ Java/Strings/Java Substring/soln.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        int start  = scan.nextInt();
+        int end    = scan.nextInt();
+        scan.close();
+        
+        System.out.println(str.substring(start, end));
+    }
+}
