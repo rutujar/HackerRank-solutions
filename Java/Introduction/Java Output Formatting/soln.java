d17affd2dc0c123a088f455be5b1bfcbc6fd5175
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-25 18:14:11 +0530

    Create soln.java

---
+++ Java/Introduction/Java Output Formatting/soln.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String s1 = scan.next();
+            int x = scan.nextInt();
+            System.out.format("%-15s%03d%n", s1, x);
+        }
+        scan.close();
+        System.out.println("================================");
+
+    }
+}
