46bb44b7c202667a333b35474374fed0e12e969a
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-25 19:07:30 +0530

    Create soln.java

---
+++ Java/Introduction/Java Loops II/soln.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int n = scan.nextInt();
+            for (int j = 0; j < n; j++) {
+                a += b * (int) Math.pow(2, j);
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+}
+
