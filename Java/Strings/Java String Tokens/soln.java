342665e83cd47f2bc9b40e37ce4ab88929cb06d7
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-25 19:16:43 +0530

    Create soln.java

---
+++ Java/Strings/Java String Tokens/soln.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Read input */
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        scan.close();
+        
+        s = s.trim(); // so that .split() works properly
+        
+        /* Check special cases */
+        if (s.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+        
+        /* Split on all non-alphabetic characters */
+        String [] words = s.split("[^a-zA-Z]+");
+        
+        /* Print output */
+        System.out.println(words.length);
+        for (String word : words) {
+            System.out.println(word);
+    }
+}
+}
