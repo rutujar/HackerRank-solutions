56fa6133c641f460a8a8d3dfc4f9c96b04b3b565
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-26 17:46:33 +0530

    Create soln.java

---
+++ Java/BigNumber/Java Primality Test/soln.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BigInteger n = scan.nextBigInteger();
+        scan.close();
+        System.out.println(n.isProbablePrime(10) ? "prime" : "not prime");
+    }
+}
