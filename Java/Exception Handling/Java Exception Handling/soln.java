53a1e4acd608ecbfefc0778bd5be6791132f6565
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-26 17:49:31 +0530

    Create soln.java

---
+++ Java/Exception Handling/Java Exception Handling/soln.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class MyCalculator {
+    /*
+    * Create the method long power(int, int) here.
+    */
+    long power(int n, int p) throws Exception {
+        if (n < 0 || p < 0) {
+            throw new Exception("n or p should not be negative.");
+        } else if (n == 0 && p == 0) {
+            throw new Exception("n and p should not be zero.");
+        } else {
+            return (long) Math.pow(n, p);
+        }
+    }
+    
+}
+
+public class Solution {
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        while (in .hasNextInt()) {
+            int n = in .nextInt();
+            int p = in .nextInt();
+            
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
