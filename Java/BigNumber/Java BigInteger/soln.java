98a0dcf4dfc0751cc859727cc81334b68c7ec1cb
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-26 17:47:00 +0530

    Create soln.java

---
+++ Java/BigNumber/Java BigInteger/soln.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BigInteger a = new BigInteger(scan.next());
+        BigInteger b = new BigInteger(scan.next());
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
+
