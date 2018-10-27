bd9de5f6d4fb2a8647784e7b241b4587bda5682e
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-27 21:10:03 +0530

    Create soln.java

---
+++ Java/Data Structures/Java 1D Array -Part 2/soln.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Solution {
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        while (T-- > 0) {
+            int n = scan.nextInt();
+            int m = scan.nextInt();
+            int [] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = scan.nextInt();
+            }
+            System.out.println(isSolvable(array, m, 0) ? "YES" : "NO");
+        }
+        scan.close();
+    }
+    
+    /* Basically a depth-first search (DFS). 
+       Marks each array value as 1 when visiting (Side-effect: alters array) */
+    private static boolean isSolvable(int[] array, int m, int i) {
+        /* Base Cases */
+        if (i < 0 || array[i] == 1) {
+            return false;
+        } else if (i + 1 >= array.length || i + m >= array.length) {
+            return true;
+        }
+        
+        array[i] = 1; // marks as visited
+
+        /* Recursive Cases (Tries +m first to try to finish game quickly) */
+        return isSolvable(array, m, i + m) || 
+               isSolvable(array, m, i + 1) || 
+               isSolvable(array, m, i - 1);
+    }
+}
+    
