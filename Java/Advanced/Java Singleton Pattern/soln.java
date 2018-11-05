c112ea6b03d397f28f3ad20ca204a0f5e4ff42fd
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-11-05 16:06:39 +0530

    Create soln.java

---
+++ Java/Advanced/Java Singleton Pattern/soln.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.reflect.Constructor;
+
+class Singleton {
+    private Singleton() {}
+    public String str;
+    private static Singleton instance = null;
+    public static Singleton getSingleInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
