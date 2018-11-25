3655a68ba0fc548565ec1e44775650ac6021df20
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-11-25 19:38:00 +0530

    Create soln.sql

---
+++ Sql/Advanced Select/Type of Triangle/soln.sql
@@ -0,0 +1,7 @@
+SELECT CASE 
+WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle' 
+WHEN A = B AND B = C THEN 'Equilateral' 
+WHEN A = B OR B = C OR A = C THEN 'Isosceles' 
+ELSE 'Scalene' 
+END 
+FROM TRIANGLES;
