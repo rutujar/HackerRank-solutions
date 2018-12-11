d07283aacc8af47dd82c29f137695dd832444bfc
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-12-11 21:26:20 +0530

    Create soln.py

---
+++ Python/Finding the percentage/soln.py
@@ -0,0 +1,12 @@
+def readScores(listOfStudents):
+    line = list(input().split())
+    avScore = sum(map(float, line[1:])) / 3
+    name = line[0]
+    listOfStudents[name] = avScore
+
+
+n = int(input())
+listOfStudents = dict()
+for i in range(n):
+    readScores(listOfStudents)
+print('%.2f' % listOfStudents[input()])
