b3a9faa41a67f724d22edb49f05742b8ab18a265
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-12-11 21:34:52 +0530

    Create soln.py

---
+++ Python/What-s Your Name/soln.py
@@ -0,0 +1,8 @@
+def print_full_name(first_name, last_name):
+    print("Hello {} {}! You just delved into python.".format(first_name, last_name))
+
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
