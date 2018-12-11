71cae1648d4aae51dbfc0baf420849112daacaf0
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-12-11 21:10:33 +0530

    Create soln.py

---
+++ Python/Write a function/soln.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    leap = False
+    if (year % 400 == 0):
+        leap = True
+    elif year % 4 == 0 and year % 100 !=0:
+        leap = True    
+    return leap
+
+year = int(raw_input())
+print is_leap(year)
