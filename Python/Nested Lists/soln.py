decd88a47bfe3869f211a7db8b7f8627130a52f7
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-12-11 21:23:09 +0530

    Create soln.py

---
+++ Python/Nested Lists/soln.py
@@ -0,0 +1,11 @@
+def secondLowestGrade(classList):
+    secondLowestScore = sorted(set(_[1] for _ in classList))[1]
+    result = sorted([_[0] for _ in classList if _[1] == secondLowestScore])
+    return result
+
+
+numberOfStudents = int(input())
+classList = []
+for i in range(numberOfStudents):
+    classList.append([str(input()), float(input())])
+print('\n'.join(secondLowestGrade(classList)))
