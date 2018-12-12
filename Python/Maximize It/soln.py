e8d8f1e1b50bac6fd8fcd4a0e4635ea082844365
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-12-12 18:17:42 +0530

    Create soln.py

---
+++ Python/Maximize It/soln.py
@@ -0,0 +1,6 @@
+from itertools import product
+
+k, m = map(int, input().split())
+n = (list(map(int, input().split()))[1:] for _ in range(k))
+results = map(lambda x: sum(i**2 for i in x) % m, product(*n))
+print(max(results))
