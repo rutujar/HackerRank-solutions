b8a2c19e6220b9b4aa4795638217ddb1a4085466
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-12-12 18:13:52 +0530

    Create soln.py

---
+++ Python/itertools.combinations/soln.py
@@ -0,0 +1,7 @@
+import itertools
+
+
+s = input().split()
+string, number = sorted(s[0]), int(s[1])
+for i in range(1, number + 1):
+    print(*list(map(''.join, itertools.combinations(string, i))), sep='\n')
