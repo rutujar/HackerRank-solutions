1b97ef8be43e5a974198001dc3fb6f55c8894b77
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-12-13 08:52:59 +0530

    Create soln.py

---
+++ Python/DefaultDict Tutorial/soln.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+
+n, m = map(int, input().split())
+d = defaultdict(list)
+for i in range(1, n + 1):
+    d[input()].append(str(i))
+for i in range(m):
+    print(' '.join(d[input()]) or -1)
