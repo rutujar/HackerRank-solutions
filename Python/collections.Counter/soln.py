e08e0c2ee322ab0aced7b237f5b888d63c6aaa4f
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-12-13 08:49:58 +0530

    Create soln.py

---
+++ Python/collections.Counter/soln.py
@@ -0,0 +1,15 @@
+import collections
+
+
+number_of_shoes = int(input())
+sizes_in_stock = collections.Counter(map(int, input().split()))
+
+total_revenue = 0
+
+for _ in range(int(input())):
+    size, price = map(int, input().split())
+    if sizes_in_stock[size]:
+        total_revenue += price
+        sizes_in_stock[size] -= 1
+
+print(total_revenue)
