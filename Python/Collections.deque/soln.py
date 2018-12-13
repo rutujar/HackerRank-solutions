3118f195ce912f44ef38cc44b650e955814369d6
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-12-13 08:57:45 +0530

    Create soln.py

---
+++ Python/Collections.deque/soln.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+
+d = deque()
+for _ in range(int(input())):
+    line = input().split()
+    if line[0] == 'append':
+        d.append(line[1])
+    elif line[0] == 'pop':
+        d.pop()
+    elif line[0] == 'popleft':
+        d.popleft()
+    elif line[0] == 'appendleft':
+        d.appendleft(line[1])
+print(*d)
