fc690442f0f9e0da598a10509370d3fbe1f77ce2
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-12-27 18:10:48 +0530

    Create soln.py

---
+++ Python/Inner and Outer/soln.py
@@ -0,0 +1,5 @@
+import numpy
+np_ar1 = numpy.array(list(map(int,input().split())))
+np_ar2 = numpy.array(list(map(int,input().split())))
+print(numpy.inner(np_ar1,np_ar2))
+print(numpy.outer(np_ar1,np_ar2))
